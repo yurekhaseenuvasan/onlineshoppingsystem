9a18869085cdf971bb3cbc2d03a19c8861103d19
unknown <yurekhaseenuvasan12@gmail.com>
2021-04-16 18:01:09 +0530

    second commit

---
+++ java.js
@@ -0,0 +1,38 @@
+var Menuitems = document.getElementById("Menuitems");
+Menuitems.style.maxHeight = "0px";
+
+    function menutoggle()
+     {
+        if(Menuitems.style.maxHeight = "0px")
+         {
+                   Menuitems.style.maxHeight = "200px";
+          }
+        else
+         {
+            Menuitems.style.maxHeight = "0px"
+            }
+            
+   }
+  
+   var LoginForm = document.getElementById("LoginForm");
+        var RegForm = document.getElementById("RegForm");
+        
+
+
+        function register() {
+            alert("Successfully signed in");
+            
+        }
+        function login() {
+            alert("Successfully logged in");
+            
+        }
+
+
+
+
+
+
+  
+
+    
